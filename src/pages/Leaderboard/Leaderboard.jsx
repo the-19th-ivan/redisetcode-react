87724c33fccf14c8ca01818f8833f4841805f87f
Ivan Ramos <ivanandesramos.io@gmail.com>
2023-10-20 20:07:35 +0800

    add announcement to leaderboard

--- src/pages/Leaderboard/Leaderboard.jsx
+++ src/pages/Leaderboard/Leaderboard.jsx
@@ -1,4 +1,4 @@
-import { Spinner, Typography } from "@material-tailwind/react";
+import { Alert, Spinner, Typography } from "@material-tailwind/react";
 import Navbar from "../../components/Navbar";
 import { Sidebar } from "../../components/Sidebar";
 import { GiDiamondTrophy, GiPodium } from "react-icons/gi";
@@ -11,6 +11,7 @@ import axios from "axios";
 export default function Leaderboard() {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [open, setOpen] = useState(true);
 
   const navigate = useNavigate();
 
@@ -49,33 +50,41 @@ export default function Leaderboard() {
             <Spinner color="indigo" />
           </div>
         ) : (
-          <div className="w-full p-10">
-            <Typography
-              variant="h4"
-              className="text-gray-900 font-montserrat font-bold flex gap-3"
-            >
-              <GiDiamondTrophy />
-              Leaderboard
-            </Typography>
+          <>
+            <Alert color="indigo" open={open} onClose={() => setOpen(false)}>
+              <Typography variant="small" className="font-bold">
+                Announcement: Top 3 players at the end of November 1, 2023 12pm
+                will earn cash prize
+              </Typography>
+            </Alert>
+            <div className="w-full p-10">
+              <Typography
+                variant="h4"
+                className="text-gray-900 font-montserrat font-bold flex gap-3"
+              >
+                <GiDiamondTrophy />
+                Leaderboard
+              </Typography>
 
-            <div className="mt-6 grid grid-cols-3 gap-6">
-              {topThree.map((user, index) => (
-                <TopRankCard key={index} data={user} rank={index + 1} />
-              ))}
+              <div className="mt-6 grid grid-cols-3 gap-6">
+                {topThree.map((user, index) => (
+                  <TopRankCard key={index} data={user} rank={index + 1} />
+                ))}
+              </div>
+
+              <Typography
+                variant="h4"
+                className="text-gray-900 font-montserrat font-bold mt-6 flex gap-3"
+              >
+                <GiPodium />
+                Rankings
+              </Typography>
+
+              <div className="mt-6">
+                <RankingTable users={users.slice(3)} />
+              </div>
             </div>
-
-            <Typography
-              variant="h4"
-              className="text-gray-900 font-montserrat font-bold mt-6 flex gap-3"
-            >
-              <GiPodium />
-              Rankings
-            </Typography>
-
-            <div className="mt-6">
-              <RankingTable users={users.slice(3)} />
-            </div>
-          </div>
+          </>
         )}
       </section>
     </main>
